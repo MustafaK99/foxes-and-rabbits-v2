d6e24d6daf3410f64516cdb78382478cb6881d53
MustafaK99 <rmk799@outlook.com>
2020-11-25 20:23:51 +0000

    Initial commit

---
+++ actor.java
@@ -0,0 +1,18 @@
+
+/**
+ * Write a description of interface actor here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public interface actor
+{
+    /**
+     * An example of a method header - replace this comment with your own
+     *
+     * @param  y a sample parameter for a method
+     * @return   the result produced by sampleMethod
+     */
+    abstract void act(Field currentField, Location location,Field updatedField);
+
+}
